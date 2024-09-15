4cbf2737558344a54b4d88669b7a1e2d3c2cc365
Antony F. Ferreira <antonyfferreira@gmail.com>
2024-09-14 23:43:30 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,106 @@
+# Análise de Marcas de Tênis Esportivos e Masculinos com Webscraping
+
+Este README fornece uma visão geral clara e detalhada do projeto, incluindo a arquitetura, a estrutura de diretórios, as instruções de instalação e uso, além dos módulos específicos para extração, transformação e visualização de dados.
+
+Uma ETL em Python para Monitoramento de Preço.
+
+## **Uma ETL em Python para Monitoramento de Preço**:
+
+Solução em Python para estratégias de pricing Temos uma pipeline e uma ETL em Python que coleta, consolida e gera insights sobre determinada cadeira de produtos.
+
+## **Necessidade**: 
+
+Precisamos entender a relevância das marcas de tênis esportivos masculinos no Mercado Livre. O objetivo desta pesquisa é avaliar como as diversas marcas são percebidas pelos consumidores e gerar métricas que respondam às seguintes perguntas:
+
+- Quais são as marcas mais frequentemente encontradas nas primeiras 20 páginas do site?
+- Qual é o preço médio dos tênis esportivos por marca?
+- Qual é o nível de satisfação dos consumidores em relação a cada marca?
+
+## Etapas do Projeto:
+
+### 1. Coleta dos Dados
+- **Objetivo:** Extrair informações sobre tênis esportivos masculinos utilizando **Python** e o framework **Scrapy** para web scraping.
+
+### 2. Tratamento dos Dados
+- **Objetivo:** Transformar e limpar os dados coletados utilizando a biblioteca **Pandas** para processamento de dados.
+
+### 3. Armazenamento dos Dados
+- **Objetivo:** Armazenar os dados tratados de forma estruturada no banco de dados **SQLite3**.
+
+### 4. Visualização dos Dados
+- **Objetivo:** Desenvolver indicadores utilizando a biblioteca **Pandas** para criar as análises e o framework **Streamlit** para visualização dos dados.
+
+## Arquitetura do Projeto
+
+Uma ETL em Python para Web Scraping
+
+- Extração - Scrapy
+- Transformação e Load - Pandas
+- Banco de dados - SQLite
+- Dashboard - Streamlit
+
+###
+
+![arquitetura](/pics/arquitetura.png)
+
+## Dashboard
+
+![dashboard](pics/app_streamlit_001.png)
+![dashboard](pics/app_streamlit_002.png)
+![dashboard](pics/app_streamlit_003.png)
+![dashboard](pics/app_streamlit_004.png)
+
+## Replicando o Projeto:
+
+- Clone o repositório:
+
+```bash
+git clone https://github.com/antonyfferreira/projeto_webscraping.git
+
+cd projeto_webscraping
+```
+- Configure a versão correta do Python com `pyenv`:
+```bash
+pyenv local 3.12.1
+python -m venv .venv
+```
+- Instale as dependências do projeto:
+```bash
+# MacOS/Linux
+source .venv/bin/activate 
+
+# Windows
+source .venv/Scripts/activate 
+
+# Instalar as dependecias/bibliotecas que consta no arquivo requirements.txt
+pip install -r requirements.txt
+```
+- Extração usando Webscraping
+```bash
+cd src/coleta/ 
+
+rm ../../data/data_mercadolivre.jsonl # remove o arquivo json caso exista
+
+scrapy crawl mercadolivre -o ../../data/data_mercadolivre.jsonl
+```
+
+- Tratamento dos dados e armazenamento:
+
+    Dentro do diretório src execute o comando
+```bash
+python src/transformacao/processing.py
+```
+- Executar o Dashboard utilizando o Streamlit
+
+    Dentro do diretório src execute o comando
+```bash
+streamlit run dashboard/app.py
+````
+
+## Documentação
+
+- Python: https://www.python.org/
+- Scrapy: https://scrapy.org/
+- Pandas: https://pandas.pydata.org/
+- SQLite: https://www.sqlite.org/
+- Streamlit: https://streamlit.io/
---
+++ src/dashboard/app.py
@@ -0,0 +1,160 @@
+import streamlit as st
+import pandas as pd
+import sqlite3
+import plotly.express as px
+
+# Título da aplicação
+
+st.markdown(
+    """
+    <style>
+    .header {
+        display: flex;
+        align-items: center;
+        padding: 10px;
+        background-color: #9cafe6  /* Ajuste a cor de fundo conforme necessário */
+    }
+    .header img {
+        height: 100px;
+        margin-right: 30px; /* Espaçamento entre a imagem e o título */
+    }
+    .header h1 {
+        margin: 0; /* Remove a margem padrão do título */
+    }
+    </style>
+    <div class="header">
+        <img src="https://logopng.com.br/logos/mercado-livre-87.png" alt="Logo">
+        <h2>Pesquisa de Mercado - Tênis Esportivos no Mercado Livre</h2>
+    </div>
+    """,
+    unsafe_allow_html=True,
+)
+
+st.divider()
+
+# Conectar ao banco de dados SQLite
+conn = sqlite3.connect("../data/ScrapeStore.db")
+
+# Carregar os dados da tabela 'mecardolivre_products' em um DataFrame
+df = pd.read_sql_query("SELECT * FROM mercadolivre_products", conn)
+
+# Encerrar conexão com o banco de dados
+conn.close()
+
+# Sub título KPIs
+st.subheader("Principais KPIs")
+
+# Definindo o layout para KPIs
+col1, col2, col3 = st.columns(3)
+
+# KPI 1: Quantidade total de produtos
+total_produtos = df.shape[0]
+col1.metric(label="Total de produtos", value=total_produtos)
+
+# KPI 2: Quantidade total de marcas
+total_marcas = df["brand"].nunique()
+col2.metric(label="Total de marcas únicas", value=total_marcas)
+
+# KPI 3: Preço médio em reais (novo preço)
+preco_medio = df["new_price"].mean()
+col3.metric(label="Preço médio dos produtos", value=f"R$ {preco_medio:.2f}")
+
+# Divisor
+st.divider()
+
+# Produtos mais encontrados
+
+# Marcas mais encontradas até a página 15
+st.subheader("Top 10 marcas mais encontradas no site das páginas 01 até 10")
+st.markdown("Distribuição da quantidade produtos por marcas")
+
+# Definindo o layout para gráficos e tabelas
+col1, col2 = st.columns([6, 6])
+
+# Contar as ocorrências de cada marca e ordenar em ordem decrescente
+qtd_marcas = df["brand"].value_counts().nlargest(10).sort_values(ascending=False)
+qtd_marcas_df = qtd_marcas.reset_index()
+qtd_marcas_df.columns = ["Marca", "Total Produtos"]
+
+# Mostrar o gráfico de barras
+fig_bar = px.bar(
+    qtd_marcas_df.sort_values(by="Total Produtos", ascending=True),
+    x="Total Produtos",
+    y="Marca",
+    orientation="h",
+    color_discrete_sequence=["#617ed7"],
+    height=400,
+)
+col1.plotly_chart(fig_bar)
+
+# Mostrar a tabela das marcas
+col2.dataframe(qtd_marcas_df)
+
+# Divisor
+st.divider()
+
+# Preço Médio
+
+# Preço médio por marcas encontradas até a página 15
+st.subheader("Top 10 marcas com maior preço médio das páginas 01 até 10")
+st.markdown("Distribuição do preço médio por marcas")
+
+# Definindo o layout para gráficos e tabelas
+col1, col2 = st.columns([6, 6])
+
+# Calcular o preço médio por marca
+df = df[df["new_price"] > 0]  # Desconsiderando marcas com valores zerados
+top_10_average_prices = df.groupby("brand")["new_price"].mean().round(2).nlargest(10)
+top_10_average_prices_df = top_10_average_prices.reset_index()
+top_10_average_prices_df.columns = ["Marcas", "Preço Médio"]
+
+
+# Criar o gráfico de barras horizontais
+fig_price_bar = px.bar(
+    top_10_average_prices_df.sort_values(by="Preço Médio", ascending=True),
+    x="Preço Médio",
+    y="Marcas",
+    orientation="h",
+    color_discrete_sequence=["#617ed7"],
+    height=400,
+)
+col1.plotly_chart(fig_price_bar)
+
+# Mostrar a tabela de preços médios
+col2.dataframe(top_10_average_prices_df)
+
+st.divider()
+
+# Satisfação por marca
+st.subheader("Top 10 marcas com melhores avaliações das páginas 01 até 10")
+st.markdown("Distribuição média das avalições por marca")
+
+# Avaliação média
+df = df[
+    df["reviews_rating_number"] > 0
+]  # Desconsiderando marcas que não tiveram avaliação
+avaliacao_marca = (
+    df.groupby("brand")["reviews_rating_number"].mean().round(3).nlargest(10)
+)
+avaliacao_marca_df = avaliacao_marca.reset_index()
+avaliacao_marca_df.columns = ["Marcas", "Avaliações Média"]
+
+
+# Criar o gráfico de barras
+fig_avaliacoes_bar = px.bar(
+    avaliacao_marca_df.sort_values(by="Avaliações Média", ascending=False),
+    x="Marcas",
+    y="Avaliações Média",
+    orientation="v",
+    color_discrete_sequence=["#617ed7"],
+)
+
+
+# Adicionar rótulos às barras
+fig_avaliacoes_bar.update_traces(texttemplate="%{y}", textposition="inside")
+
+# Define o ângulo dos rótulos do eixo x para 0 graus
+fig_avaliacoes_bar.update_layout(xaxis_tickangle=0)
+
+# Exibir o gráfico no Streamlit
+st.plotly_chart(fig_avaliacoes_bar)
